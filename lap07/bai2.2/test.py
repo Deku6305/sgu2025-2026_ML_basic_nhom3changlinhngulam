4c8e948001b206b758af043db05f3a17c50bbf8b
Deku6305 <tuandat6305@gmail.com>
2025-10-29 13:12:11 +0700

    Ly thuyet bai 2.2

---
+++ lap07/bai2.2/test.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+data = pd.read_csv("data/iris.csv")
+print(data.head())
+X = data.iloc[:, :-1]  
+y = data.iloc[:, -1]  
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42, stratify=y
+)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+svm_model = SVC(kernel='rbf', C=1.0, gamma='scale')
+svm_model.fit(X_train_scaled, y_train)
+y_pred = svm_model.predict(X_test_scaled)
+print("\n Độ chính xác:", accuracy_score(y_test, y_pred))
+print("\n Báo cáo chi tiết:\n", classification_report(y_test, y_pred))
+print("\n Ma trận nhầm lẫn:\n", confusion_matrix(y_test, y_pred))
